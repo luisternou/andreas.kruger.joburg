28c903d3b137c9802416653265bad72d2a6eba1a
Andreas Krueger <a01649986@unet.univie.ac.at>
2022-02-07 12:44:18 +0200

    test

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Andi's Website</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>  
+    <script src="form.js"></script>  
   </head>
   <body>
 
@@ -146,17 +148,29 @@
       <h3>Thanks, got your email</h3>
 
       <?php else: ?>
-        <form method="post" name="contactform" action="index.php">
-
-            Enter Name:	<input type="text" name="name">
-            
-            Enter Email Address:	<input type="text" name="email">
-            Enter Subject: <input type="text" name="subject">
-            
-            Enter Message:	<textarea name="message"></textarea>
-            
-            <input type="submit" value="Send Form">
-            </form>
+        <div class="container">
+        <form action="webform.php" method="POST" class="form">
+            <div class="form-group">
+                <label for="name" class="form-label">Your Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Jane Doe" tabindex="1" required>
+            </div>
+            <div class="form-group">
+                <label for="email" class="form-label">Your Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="jane@doe.com" tabindex="2" required>
+            </div>
+            <div class="form-group">
+                <label for="subject" class="form-label">Subject</label>
+                <input type="text" class="form-control" id="subject" name="subject" placeholder="Hello There!" tabindex="3" required>
+            </div>
+            <div class="form-group">
+                <label for="message" class="form-label">Message</label>
+                <textarea class="form-control" rows="5" cols="50" id="message" name="message" placeholder="Enter Message..." tabindex="4"></textarea>
+            </div>
+            <div>
+                <button type="submit" class="btn">Send Message!</button>
+            </div>
+        </form>
+    </div>
 
       <?php endif; ?>
 
